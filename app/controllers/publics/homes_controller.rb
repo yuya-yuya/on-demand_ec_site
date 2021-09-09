27bf85e79de7ed07d9aaaf7b272f94369c90ee21
yuya-yuya <yytyfp5115@icloud.com>
2021-09-09 10:42:49 +0900

    addittional commit

--- app/controllers/publics/homes_controller.rb
+++ app/controllers/publics/homes_controller.rb
@@ -1,4 +1,9 @@
 class Publics::HomesController < ApplicationController
     def top
     end
+
+    def about
+        @genres = Genre.all
+        @items = Item.all.order(created_at: :desc).last(4)
+    end
 end
