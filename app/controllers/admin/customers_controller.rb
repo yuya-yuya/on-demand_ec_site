1570f431a661d13b84e06d2e6ffaceab3f3dd0b7
yuya-yuya <yytyfp5115@icloud.com>
2021-09-20 06:17:09 +0900

    additional commit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,4 +2,28 @@ class Admin::CustomersController < ApplicationController
     def index
         @customers = Customer.all
     end
+
+    def show
+        @customer = Customer.find(params[:id])
+    end
+
+    def edit
+        @customer = Customer.find(params[:id])
+    end
+
+    def update
+        @customer = Customer.find(params[:id])
+        @customer.is_subscribed = params[:customer][:is_subscribed].to_i
+        if @customer.update(custmer_params)
+            redirect_to admin_customer_path(@customer.id)
+        else
+            flash[:error] = "必須項目を記入してください。"
+            render :edit
+        end
+    end
+
+    private
+    def custmer_params
+         params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email,)
+    end
 end
