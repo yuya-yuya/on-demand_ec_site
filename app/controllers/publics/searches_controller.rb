0444f3c5a0fb354bfb696f666474cfac4b6f60fb
yuya-yuya <yytyfp5115@icloud.com>
2021-09-28 03:44:25 +0900

    additional commit

--- app/controllers/publics/searches_controller.rb
+++ app/controllers/publics/searches_controller.rb
@@ -1,19 +1,19 @@
 class Publics::SearchesController < ApplicationController
   def top
     if params[:order_sort] == "0"
-      @items = Item.where("genre_id LIKE ?", "#{params[:search]}")
+      @items = Item.where("genre_id LIKE ?", "#{params[:search]}").page(params[:page]).per(8)
       @genres = Genre.all
       render "publics/items/index"
     else
       if params[:search].present?
         if params[:how] == "0"
-            @items = Item.where("name LIKE ?", "#{params[:search]}")
+            @items = Item.where("name LIKE ?", "#{params[:search]}").page(params[:page]).per(8)
         elsif params[:how] == "1"
-            @items = Item.where("name LIKE ?", "#{params[:search]}%")
+            @items = Item.where("name LIKE ?", "#{params[:search]}%").page(params[:page]).per(8)
         elsif params[:how] == "2"
-            @items =I tem.where("name LIKE ?", "%#{params[:search]}")
+            @items =I tem.where("name LIKE ?", "%#{params[:search]}").page(params[:page]).per(8)
         else
-            @items = Item.where(['name LIKE ?', "%#{params[:search]}%"])
+            @items = Item.where(['name LIKE ?', "%#{params[:search]}%"]).page(params[:page]).per(8)
         end
         render "publics/items/index"
         #renderを使うことでコントローラーを経由させない。コントローラーを経由すると変数が変わる。
