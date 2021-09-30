8be9321dec2a8e8f6e2e51d5c4b32c9dfbd27927
yuya-yuya <yytyfp5115@icloud.com>
2021-09-30 22:30:14 +0900

    additional commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '518230a1b7ea41686e741be36c0864622890874882542e25eedd6d9d57796b9c2bfb7f61bd1c515c4a8ec4f20b7afd27e58e9ef8ecfef5d0b4ce696b7e920cee'
