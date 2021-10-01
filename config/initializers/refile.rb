017b6499b89193a7e08d416b5b5cd22cc23cb9c9
yuya-yuya <yytyfp5115@icloud.com>
2021-10-01 13:54:45 +0900

    additional commit

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,12 @@
-require "refile/s3"
+require 'refile/s3'
 
-aws = {
-  access_key_id: "AKIATGUNARVZVNDLHKFS",
-  secret_access_key: "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",
-  region: "ap-northeast-1",
-  bucket: "nagano-cake",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+if Rails.env.production? 
+  aws = {
+    access_key_id: "AKIATGUNARVZVNDLHKFS",
+    secret_access_key: "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",
+    region: 'ap-northeast-1', 
+    bucket: "nagano-cake",
+  }
+  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+  Refile.store = Refile::S3.new(prefix: 'store', **aws)
+end
